c2b5a026edfc02eb58d67ed5f08c346f54fbafa5
Gromit Park <bemore.one@gmail.com>
2019-07-02 21:06:28 +0900

    Update ofApp.cpp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -17,36 +17,36 @@ void ofApp::update(){
     cv::cvtColor(img, img, CV_BGR2GRAY);
     cv::Mat img2, img3;
 
-    // hough t
+
     cv::Mat dst, cdst;
     cv::Canny(img, dst, threshold-20, threshold+20);
     toOf(dst, after);
     after.update();
     cvtColor(dst, cdst, CV_GRAY2BGR);
-    //ofVec2f lines;
+
     std::vector<cv::Vec2f> lines;
     cv::HoughLines(dst, lines, 1, CV_PI/180, threshold2);
     
-    // 선 그리기
+    
     cv::Mat result(dst.rows, dst.cols, CV_8U, cv::Scalar(255));
     std::cout << "Lines detected: " << lines.size() << std::endl;
     
-    // 선 벡터를 반복해 선 그리기
+    
     std::vector<cv::Vec2f>::const_iterator it= lines.begin();
     while (it!=lines.end()) {
-        float rho = (*it)[0]; // 첫 번째 요소는 rho 거리
-        float theta = (*it)[1]; // 두 번째 요소는 델타 각도
-        if (theta < PI/4. || theta > 3.*PI/4.) { // 수직 행
-            cv::Point pt1(rho/cos(theta), 0); // 첫 행에서 해당 선의 교차점
+        float rho = (*it)[0]; 
+        float theta = (*it)[1]; 
+        if (theta < PI/4. || theta > 3.*PI/4.) { 
+            cv::Point pt1(rho/cos(theta), 0); 
             cv::Point pt2((rho-result.rows*sin(theta))/cos(theta), result.rows);
-            // 마지막 행에서 해당 선의 교차점
-            cv::line(cdst, pt1, pt2, cv::Scalar(255), 2); // 하얀 선으로 그리기
             
-        } else { // 수평 행
-            cv::Point pt1(0,rho/sin(theta)); // 첫 번째 열에서 해당 선의 교차점
+            cv::line(cdst, pt1, pt2, cv::Scalar(255), 2); 
+            
+        } else { 
+            cv::Point pt1(0,rho/sin(theta)); 
             cv::Point pt2(result.cols,(rho-result.cols*cos(theta))/sin(theta));
-            // 마지막 열에서 해당 선의 교차점
-            cv::line(cdst, pt1, pt2, cv::Scalar(255), 2); // 하얀 선으로 그리기
+            
+            cv::line(cdst, pt1, pt2, cv::Scalar(255), 2); 
         }
         std::cout << "line: (" << rho << "," << theta << ")\n";
         ++it;
